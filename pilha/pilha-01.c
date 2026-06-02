9b196e8b693d1d1264aaed44251a88b5f0b1d71a
Emanuelle Duque <emanuelleduquesantana@gmail.com>
2026-06-02 00:13:19 +0000

    pilha atividade

---
+++ pilha/pilha-01.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef struct {    
+    int dados[MAX]; // os elementos
+    int topo;       // indice do topo
+} pilha;            // -1 = vazia
+
+void inicializar(pilha *p) {
+    p->topo = -1;
+}
+
+int estaVazia(pilha *p) {
+    return p->topo == -1;
+}
+
+int estaCheia(pilha *p){
+    return p->topo == MAX - 1;
+}
+
+void push(pilha *p, int valor){
+    if (estaCheia(p)) return;
+    p->dados[++p->topo] = valor;
+}
+
+int pop(pilha *p){
+    if (estaVazia(p)) return -1;
+    return p->dados[p->topo];
+}
+
+// Uso: 
+
+int main()
+{
+pilha p;
+inicializar(&p);
+push(&p, 10); push(&p, 20);
+printf("%d\n", pop(&p)); // 20
+
+return 0;
+}
+
