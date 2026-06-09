b40f7d2b651d754ab67e54cb353a60f794a2272e
Emanuelle Duque <emanuelleduquesantana@gmail.com>
2026-06-09 00:31:17 +0000

    fila

---
+++ fila/fila-01.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef struct {
+    int dados [MAX];
+    int inicio;
+    int fim;
+    int tamanho;
+} fila;
+
+void inicializar(fila *f) {
+    f->inicio = f->fim = f->tamanho = 0;
+}
+int estaVazia(fila *f) {
+    return f->tamanho == 0;
+}
+int estaCheia(fila *f) {
+    return f->tamanho == MAX;
+}
+void enqueue(fila *f, int valor) {
+    if (estaCheia(f)) return;
+    f->dados[f->fim] = valor;
+    f->fim = (f->fim + 1) % MAX;
+    f->tamanho++;
+}
+int dequeue(fila *f) {
+    if (estaVazia(f)) return -1;
+    int v = f->dados[f->inicio];
+    f->inicio - (f->inicio + 1) % MAX;
+    f->tamanho--;
+    return v;
+}
+int front(fila *f){
+    if (estaVazia(f)) return -1;
+    return f->dados[f->inicio];
+}
+
+int main()
+{
+fila f; inicializar (&f);
+enqueue(&f, 101); enqueue(&f, 102); enqueue(&103); enqueue(&104);
+printf("%d\n", dequeue(&f));
+enqueue (&103); enqueue (&104); enqueue (&105); enqueue (&106);
+
+while (estaVazia(f))
+    printf("%d\n", dequeue(%f));
+
+    return 0;
+}
